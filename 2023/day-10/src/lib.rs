89182f6529c8d873b63be324ee3d78f608a68e80
JB Griesner <jean.benoit.griesner@coralogix.com>
2023-12-09 11:57:26 +0100

    day 9

--- 2023/day-10/src/lib.rs
+++ 2023/day-10/src/lib.rs
@@ -5,21 +5,19 @@ pub mod part2;
 mod tests {
     use super::*;
 
-    const INPUT1: &str = "0 3 6 9 12 15
-1 3 6 10 15 21
-10 13 16 21 30 45";
+    const INPUT1: &str = "";
 
-    const INPUT2: &str = INPUT1;
+    const INPUT2: &str = "";
 
     #[test]
     fn part1() {
         let result = part1::process(INPUT1);
-        assert_eq!(result, "114");
+        assert_eq!(result, "");
     }
 
     #[test]
     fn part2() {
         let result = part2::process(INPUT2);
-        assert_eq!(result, "2");
+        // assert_eq!(result, "");
     }
 }
--- 2023/day-9/src/part1.rs
+++ 2023/day-9/src/part1.rs
@@ -1,4 +1,52 @@
+use nom::{
+    branch::alt,
+    bytes::complete::tag,
+    character::complete::{digit1, newline, space1},
+    multi::separated_list1,
+    sequence::preceded,
+    IResult,
+};
+
+fn negative_digit1(input: &str) -> IResult<&str, i64> {
+    let (input, n) = preceded(tag("-"), digit1)(input)?;
+    Ok((input, ["-", n].concat().parse().unwrap()))
+}
+
+fn positive_digit1(input: &str) -> IResult<&str, i64> {
+    let (input, n) = digit1(input)?;
+    Ok((input, n.parse().unwrap()))
+}
+
+fn neg_or_pos(input: &str) -> IResult<&str, i64> {
+    alt((positive_digit1, negative_digit1))(input)
+}
+
+fn line(input: &str) -> IResult<&str, Vec<i64>> {
+    separated_list1(space1, neg_or_pos)(input)
+}
+
+fn parse(input: &str) -> Vec<Vec<i64>> {
+    separated_list1(newline, line)(input).unwrap().1
+}
+
+fn solve(sequence: &Vec<i64>) -> i64 {
+    fn close(seq: &Vec<i64>, lasts: &mut Vec<i64>) -> i64 {
+        lasts.push(*seq.last().unwrap());
+        let next_seq = &seq[..]
+            .windows(2)
+            .map(|pair| pair[1] - pair[0])
+            .collect::<Vec<i64>>();
+        if next_seq.iter().all(|&x| x == 0) {
+            lasts.iter().map(|x| x.clone()).sum()
+        } else {
+            close(next_seq, lasts)
+        }
+    }
+    let mut init = vec![];
+    close(sequence, &mut init)
+}
+
 pub fn process(s: &str) -> String {
-  let rez = "one";
-  rez.to_string()
+    let lines = parse(s);
+    lines.iter().map(solve).sum::<i64>().to_string()
 }
